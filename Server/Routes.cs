890c83d6da3ba4f736485f4ef9905c8f35ffe278
Crocodilys <dmitry.stabrovsky@gmail.com>
2014-01-09 16:49:49 +0300

    First commit

---
+++ Server/Routes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Routes
+    {
+        public static string  GetRoutes(string parameters)
+        {
+
+            parameters = parameters.Replace("\0", string.Empty);
+
+            if(Regex.IsMatch(parameters, @"^auth?.*"))
+            {
+                Match match = Regex.Match(parameters, @"login=(.*)&password=(.*)");
+                string name = "";
+                string password = "";
+                if (match.Success)
+                {
+                    name = match.Groups[1].Value;
+                    password = match.Groups[2].Value;
+                }
+                
+                int i = 0;
+            }
+            
+            return "page 404"; 
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main()
+        {
+            // Create an instance of the TcpListener class.
+            TcpListener tcpListener = null;
+            IPAddress ipAddress = Dns.GetHostEntry("localhost").AddressList[0];
+            try
+            {
+                // Set the listener on the local IP address 
+                // and specify the port.
+                tcpListener = new TcpListener(ipAddress, 3381);
+                tcpListener.Start();
+                Console.WriteLine("Waiting for a connection...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            while (true)
+            {
+                // Always use a Sleep call in a while(true) loop 
+                // to avoid locking up your CPU.
+                Thread.Sleep(10);
+                // Create a TCP socket. 
+                // If you ran this server on the desktop, you could use 
+                // Socket socket = tcpListener.AcceptSocket() 
+                // for greater flexibility.
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                // Read the data stream from the client. 
+                byte[] bytes = new byte[256];
+                NetworkStream stream = tcpClient.GetStream();
+                stream.Read(bytes, 0, bytes.Length);
+
+                TcpClient mscClient;
+                string mstrMessage;
+                string mstrResponse;
+                byte[] bytesSent;
+
+                // Handle the message received and  
+                // send a response back to the client.
+                mstrMessage = Encoding.UTF8.GetString(bytes);// Encoding.ASCII.GetString(bytesReceived, 0, bytesReceived.Length);
+                mscClient = tcpClient;
+                mstrMessage = mstrMessage.Substring(0, 255);
+                Routes.GetRoutes(mstrMessage);
+            }
+        }
+    }
+}
