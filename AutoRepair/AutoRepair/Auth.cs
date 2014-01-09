d8afa9ae2ba1fc5a77efc7ce7bd47d8129eac9b4
zhilinskyegor <zhilinskyegor@gmail.com>
2014-01-09 03:40:04 +0200

    Egor is connected

---
+++ AutoRepair/AutoRepair/Auth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoRepair
+{
+    public partial class Auth : Form
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            Connector.getInstance().Send("auth?login=" + textBoxLogin.Text + "&password=" +textBoxPassword.Text);
+            //MessageBox.Show(textBoxLogin.Text + textBoxPassword.Text);
+        }
+
+
+    }
+}
---
+++ AutoRepair/AutoRepair/Connector.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace AutoRepair
+{
+    class Connector
+    {
+
+        private static volatile Connector instance = null;
+
+        private Socket s;
+ 
+	    private Connector() 
+        {
+            string host;
+            int port = 3381;
+
+            host = Dns.GetHostName();
+            
+
+        }
+
+        public bool ConnectSocket(string server, int port)
+        {
+            this.s = null;
+            IPHostEntry hostEntry = null;
+
+            // Get host related information.
+            hostEntry = Dns.GetHostEntry("localhost");
+
+            // Loop through the AddressList to obtain the supported AddressFamily. This is to avoid 
+            // an exception that occurs when the host IP Address is not compatible with the address family 
+            // (typical in the IPv6 case). 
+            foreach (IPAddress address in hostEntry.AddressList)
+            {
+                try
+                {
+                    IPEndPoint ipe = new IPEndPoint(address, port);
+                    Socket tempSocket =
+                        new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                    tempSocket.Connect(ipe);
+
+                    if (tempSocket.Connected)
+                    {
+                        s = tempSocket;
+                        return true;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                catch
+                {
+                    return false;
+                }
+
+            }
+            return false;
+        }
+
+        public bool Send(String message)
+        {
+            if (s.Send(GetBytes(message)) != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private byte[] GetBytes(string str)
+        {
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(str);//.Unicode.GetBytes(str);
+            return bytes;
+        }
+
+	    public static Connector getInstance() 
+        {
+		    if (instance == null) 
+            {
+                instance = new Connector();
+            }
+        return instance;
+	    }
+
+    }
+}
